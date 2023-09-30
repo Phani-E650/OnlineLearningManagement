78b7404489774888b43f7f4f7a77ec7c34f9e317
Ayyappa <pinnepalliayyappa@gmail.com>
2023-09-30 19:59:19 +0530

    senting mail

--- Frontend/src/app/admin/admin.component.ts
+++ Frontend/src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LogoutService } from '../logout.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-admin',
@@ -8,7 +9,7 @@ import { LogoutService } from '../logout.service';
     styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-    constructor(private router: Router,private logoutService: LogoutService) {}
+    constructor(private router: Router,private logoutService: LogoutService,private http: HttpClient) {}
 
     showCreateUserForm() {
         this.router.navigate(['/create']);
@@ -16,6 +17,21 @@ export class AdminComponent {
 
     showCreateMultipleUsersForm() {
         this.router.navigate(['/upload-excel']);
+    }
+    sentmail(){
+        console.log("hi");
+        this.http.post('http://localhost:8080/api/admin/sentmail',null).subscribe(
+            (response: any) => {
+                console.log("hiiiiii");
+                if (response.message === 'Mails sent successfully.') {
+                    console.log('Registration email sent to successful');
+                  } else {
+                    console.error('Admin creation failed.');
+                  }
+            },
+            (error: any) => {
+                console.error('An error occurred while creating the admin:', error);
+              })
     }
 
     logout() {
--- elearn/elearn/src/main/java/com/application/demo/controller/UserController.java
+++ elearn/elearn/src/main/java/com/application/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.application.demo.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
@@ -104,7 +105,24 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
         }
     }
+    @PostMapping("/admin/sentmail")
+    public ResponseEntity<String> sentMailRequest() {
+        try {
+        	List<UserTemp> mailsent=userTempRepository.findAll();
+        	for(UserTemp temp:mailsent) {
+        		if(temp.getStatus().equals("Req Sent")) {
+        			String registrationToken = UUID.randomUUID().toString();
+        			 emailService.sendRegistrationEmail(temp.getEmail(), registrationToken);
+        		}
+        	}
 
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Mails sent successfully.");
+        } catch (Exception e) {
+        	System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
+        }
+    }
 
     
     
